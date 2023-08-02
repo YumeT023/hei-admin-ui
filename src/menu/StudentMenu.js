6d705c79ef13fbca891f81b4a01964190a9e6a6b
FaniryKeziah <hei.keziah.2@gmail.com>
2023-08-02 17:00:30 +0300

    fix: refactor fees item menu

--- src/menu/StudentMenu.js
+++ src/menu/StudentMenu.js
@@ -8,14 +8,16 @@ import authProvider from '../providers/authProvider'
 
 export const StudentMenu = () => {
   const notify = useNotify()
+
   const notifyNotImplemented = () => notify('En cours de développement. Ce qui présage quelques exercices pour vous 😉', { type: 'warning' })
+  
   const whoamiId = authProvider.getCachedWhoami().id
+
   return (
-    // TODO: move 'height' sx in haTheme
     <Box sx={{ flex: 1 }}>
       <MultiLevelMenu variant='categories' sx={{ width: '200px', backgroundColor: '#2c393a', color: 'white' }}>
         <MenuItemCategory to='/profile' name='profile' label='Mon profil' icon={<AccountCircle />} />
-        <MenuItemCategory to={whoamiId ? `/students/${authProvider.getCachedWhoami().id}/fees` : '/'} name='fees' label='Frais' icon={<AttachMoney />} />
+        <MenuItemCategory to={whoamiId ? `/students/${whoamiId}/fees` : '/'} name='fees' label='Frais' icon={<AttachMoney />} />
         <MenuItemCategory to='/' name='student-grades' label='Notes' icon={<Receipt />} onClick={notifyNotImplemented} />
       </MultiLevelMenu>
     </Box>
