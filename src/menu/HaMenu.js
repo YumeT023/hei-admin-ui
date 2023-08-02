3e4678ccbd77b3df818942ee1201077a274c1580
Yume <hei.hajatiana@gmail.com>
2023-08-02 11:25:31 +0300

    refactor(style)!: HaMenu

--- src/menu/HaMenu.js
+++ src/menu/HaMenu.js
@@ -3,20 +3,48 @@ import StudentMenu from './StudentMenu'
 import ManagerMenu from './ManagerMenu'
 import TeacherMenu from './TeacherMenu'
 import { WhoamiRoleEnum } from '../gen/haClient'
+import { Box, Button } from '@mui/material'
+import { Logout as LogoutIcon } from '@mui/icons-material'
 
-const HaMenu = () => {
-  const role = authProvider.getCachedWhoami().role
+const Logout = () => (
+  <Box sx={{ textAlign: 'center', bgcolor: '#2c393a', color: 'white' }}>
+    <Button variant='text' endIcon={<LogoutIcon />} sx={{ color: 'white', border: '0px' }} onClick={authProvider.logout}>
+      Se déconnecter
+    </Button>
+  </Box>
+)
 
-  if (role === WhoamiRoleEnum.Student) {
-    return <StudentMenu />
+const HaMenuWrapper = ({ children }) => (
+  <Box
+    sx={{
+      height: '100vh',
+      justifyContent: 'space-between',
+      flexDirection: 'column',
+      display: 'flex'
+    }}
+  >
+    {children}
+    <Logout />
+  </Box>
+)
+
+const Menu = ({ role }) => {
+  switch (role) {
+    case WhoamiRoleEnum.Student:
+      return <StudentMenu />
+    case WhoamiRoleEnum.Manager:
+      return <ManagerMenu />
+    case WhoamiRoleEnum.Teacher:
+      return <TeacherMenu />
+    default:
+      throw new Error('Unexpected role ' + role)
   }
-  if (role === WhoamiRoleEnum.Manager) {
-    return <ManagerMenu />
-  }
-  if (role === WhoamiRoleEnum.Teacher) {
-    return <TeacherMenu />
-  }
-  return null
 }
+
+const HaMenu = () => (
+  <HaMenuWrapper>
+    <Menu role={authProvider.getCachedWhoami().role} />
+  </HaMenuWrapper>
+)
 
 export default HaMenu
