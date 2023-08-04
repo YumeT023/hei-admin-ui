dda89f167e29b551c38b7d2e7acbe0fd86301eb0
HidDen-MaChinA <hei.franco.3@gmail.com>
2023-08-04 19:43:43 +0200

    refactor:changed isPaid location to utils folder

--- src/operations/fees/utils.js
+++ src/operations/fees/utils.js
@@ -1,14 +1,10 @@
-import { Datagrid, FunctionField, ShowButton, TextField } from 'react-admin'
+import { Datagrid, FunctionField, ShowButton } from 'react-admin'
 import React from 'react'
-import { prettyPrintMoney, CustomDateField, commentFunctionRenderer } from '../utils'
-import rowStyle from './byStatusRowStyle'
-import { mainTheme } from '../../haTheme'
-import {PriceCheck, ReportProblem, WarningAmber} from "@mui/icons-material"
-
+import { prettyPrintMoney, CustomDateField, commentFunctionRenderer, isPaid } from '../utils'
 export const FeesListItems = () => {
   return (
     <Datagrid bulkActionButtons={false} rowClick={id => `/fees/${id}/show`}>
-      <FunctionField source='comment' render={record => isPaid(record.status)} label='status' />
+      <FunctionField render={record => isPaid(record.status)} label='status' />
       <CustomDateField source='due_datetime' label='Date limite' showTime={false} />
       <FunctionField source='comment' render={commentFunctionRenderer} label='Commentaire' />
       <FunctionField label='Reste à payer' render={record => prettyPrintMoney(record.remaining_amount)} textAlign='right' />
@@ -16,12 +12,4 @@ export const FeesListItems = () => {
       <ShowButton basePath='/fees' />
     </Datagrid>
   )
-}
-
-const isPaid = (input) =>{
-  switch(input){
-    case "LATE":return  <WarningAmber color='warning'/>
-    case "UNPAID":return<ReportProblem color='error'/>
-    case "PAID":return <PriceCheck  color='success'/>
-  }
 }
