93f074beea62b3ea24cf6a26c7f5c75e72649331
Maheri29 <hei.mahery.2@gmail.com>
2023-08-08 13:51:39 +0300

    feat: update profile page

--- src/haTheme.js
+++ src/haTheme.js
@@ -15,29 +15,77 @@ export const mainTheme = createTheme({
     fontSize: 15
   },
   //shadows: Array(25).fill('none')
+  //Couleur HEI jaune : #FCAF3B , rgb(252,175,59)
+  //Couleur HEI bleu : #001948 , rgb(0,25,72)
+  //Couleur HEI noir : #14171C , rgb(20,23,28)
+  //palette couleur gris : #ECF2F8 , rgb(236,242,248)
+  
+  
+  
 
   components: {
     RaSimpleShowLayout: {
       styleOverrides: {
         root: {
+          //border : "1px solid",
           "& .RaSimpleShowLayout-stack": {
-            backgroundColor: "rgb(256,253,254)",
+            margin: "0 auto",
+            padding : "10px", 
+            fontFamily: 'Arial, sans-serif',
+            marginLeft: "5vh",
+            marginTop: "3vh",
+            marginBottom: "3vh",
+          },
+          "& .RaSimpleShowLayout-row": {
+            width: "12%",
+            display: "inline-block",
+          },
+          "& .MuiStack-root.ra-field.ra-field-first-name, .MuiStack-root.ra-field.ra-field-last-name": {
+            display: "inline-block",
+            width: "48%",
+            marginRight: "2%",
+          },
+          "& .MuiStack-root.ra-field.ra-field-birth-date, .MuiStack-root.ra-field.ra-field-address, .MuiStack-root.ra-field.ra-field-email, .MuiStack-root.ra-field.ra-field-entrance-date": {
+            width: "12%",
+            display: "inline-block",
+          },
+          "& .css-8l66u2-MuiStack-root-RaLabeled-root .RaLabeled-label": {
+            fontFamily: "fantasy",
+            fontSize: "inherit",
+            color: "#001948", 
+          },
+          "& .MuiStack-root.ra-field.ra-field-ref.RaSimpleShowLayout-row.css-8166u2-Muistack-root-RaLabeled-root": {
             width: "25%",
-            margin: "0 auto", 
-            textAlign: "center",
-            fontFamily: 'Arial, sans-serif'
-          }
+            border: "1px solid #ECF2F8", 
+          },
         },
       },
     },
-    RaBreadcrumb: { 
+    RaLayout : {
+      styleOverrides: {
+        root:{
+          "& .RaLayout-content": {
+            backgroundColor : "#ECF2F8",
+          }
+        }
+      }
+    },
+    RaShow: {
       styleOverrides: {
         root: {
-          "& .RaBreadcrumb-list": {
-            backgroundColor: 'rgb(79,60,201)', 
+          "& .RaShow-card": {
+            marginLeft: '5vh',
+            marginRight: '5vh',
           }
-        },
-      },
+        }
+      }
     },
+    MuiLink: {
+      styleOverrides: {
+        root:{
+          color : 'black',
+        }
+      }
+    }
   },
 });
