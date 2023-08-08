6b0ddb6abbfd7273ba64067127a5ae1d0a4073cc
FaniryKeziah <hei.keziah.2@gmail.com>
2023-08-09 01:26:19 +0300

    feat: add props for Input

--- src/operations/utils/Input.js
+++ src/operations/utils/Input.js
@@ -2,12 +2,22 @@ import { TextInput } from 'react-admin'
 import { Box, Typography } from '@mui/material'
 
 const Input = props => {
-  const { source, placeholder, type } = props
+  const { source, placeholder, type, name, disabled, validate } = props
 
   return (
     <Box>
       <Typography sx={{ fontWeight: 'bold' }}>{placeholder}</Typography>
-      <TextInput source={source} placeholder={placeholder} variant='outlined' label='' type={type} sx={{ width: '500px', height: '50px' }} />
+      <TextInput
+        source={source}
+        placeholder={placeholder}
+        variant='outlined'
+        label=''
+        type={type}
+        name={name}
+        disabled={disabled}
+        validate={validate}
+        sx={{ width: '500px', height: '50px' }}
+      />
     </Box>
   )
 }
